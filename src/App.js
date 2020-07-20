1b655428896f39fb4ea152aabb25b0ef92042bee
yirano <yirano@protonmail.ch>
2020-07-20 19:50:08 -0400

    Completed Forms

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialFormState = {
+  name: '',
+  email: ''
 }
 
-export default App;
+function App() {
+  const [data, setData] = useState([])
+  const [form, setForm] = useState(initialFormState)
+
+  const handleChange = e => {
+    console.log('Value of input --> ', e.target.value)
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setData([...data, form])
+    setForm(initialFormState)
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          onChange={handleChange}
+          value={form.name}
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          onChange={handleChange}
+          value={form.email}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+      {data.map(item => {
+        return (
+          <>
+            <p>{item.name}</p>
+            <p>{item.email}</p>
+          </>
+        )
+      })}
+    </>
+  )
+}
+
+export default App
